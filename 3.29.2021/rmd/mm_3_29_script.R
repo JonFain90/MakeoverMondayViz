fa6715c798c621437490217d6cc7c51cd2ffc1f0
Jon Fain <jonfain90@gmail.com>
2021-03-30 16:44:55 -0600

    1st attempt

---
+++ 3.29.2021/rmd/mm_3_29_script.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(httr)
+library(readxl)
+library(lubridate)
+library(ggthemes)
+
+
+#import data 
+GET("https://query.data.world/s/5x54n3of3j55xzsno6y3biyc3plzn3", write_disk(tf <- tempfile(fileext = ".xlsx")))
+df <- read_excel(tf)
+
+
+#explore data
+summary(df)
+lapply(df, class)
+
+
+
+#transform data 
+
+# Separate year from datetime:
+df$Year <- format(df$Month, format="%Y")
+
+dfg <- df %>%
+  group_by(Month) %>%
+  summarise(sum_imports_bil = sum(`Imports (£m)`) / 1000, sum_exports_bil = sum(`Exports (£m)`) / 1000)
+
+b <- dfg %>%
+  filter(dfg$Month >= as.Date("2020-12-01"))
+
+# set event
+d <- dfg %>%
+  select(Month) %>%
+  filter(Month == as.Date("2020-12-01")) %>%
+  mutate(event = "Brexit Formally Takes Place")
+
+
+#visualize data
+
+
+ggplot(dfg, aes(x = Month, y = sum_exports_bil)) +
+  geom_line(size = 1, color = "blue") +
+geom_line(data = b, aes(x = Month, y = sum_exports_bil), color = "red", size = 1) +
+  labs(title = "Exports to the EU Have Plunged Since Brexit (£bn)", 
+       subtitle = NULL, 
+       x = NULL, 
+       y = "£B")  +
+  geom_vline(data=d, aes(xintercept=Month), color="black", linetype="dotted", size = 1) +
+  geom_text(data=d, mapping=aes(x=Month, y=8, label=event), size=4, angle=90, vjust=-0.9, hjust=-1.2) +
+  theme_fivethirtyeight()
+
+
+
